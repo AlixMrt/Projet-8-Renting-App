1dd5e6318ad422b7c2cab60941d35c385e25fdbd
AlixMrt <alix.mrt@proton.me>
2024-10-10 13:23:52 +0200

    tried modifying the router

--- src/router.js
+++ src/router.js
@@ -10,15 +10,19 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 
+// Define the basename for the router
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/Projet-8-Renting-App" element={<RootLayout />}>
+    <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="rentedProperty/:id" element={<RentedProperty />} />
       <Route path="*" element={<Error />} />
     </Route>
-  )
+  ),
+  {
+    basename: "/Projet-8-Renting-App", // Set the basename to your repository name
+  }
 );
 
 export default router;
